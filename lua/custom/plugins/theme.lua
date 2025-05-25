fb390478b19b4b52767d90d1b60b41d347ddf6be
RasmusBoNielsen <120665429+RasmusBoNielsen@users.noreply.github.com>
2025-05-25 23:59:37 +0200

    Add files via upload

--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -1,16 +1,29 @@
 -- Using Lazy
 return {
+  -- {
+  --   'sainnhe/everforest',
+  --   lazy = false,
+  --   --priority = 1000
+  --   priority = 0,
+  --   config = function()
+  --     -- Optionally configure and load the colorscheme
+  --     -- directly inside the plugin declaration.
+  --     vim.g.everforest_enable_italic = true
+  --     vim.g.everforest_background = 'hard'
+  --     vim.cmd.colorscheme 'everforest'
+  --   end,
+  -- },
   {
-    'sainnhe/everforest',
+    'sainnhe/gruvbox-material',
     lazy = false,
-    --priority = 1000
     priority = 0,
     config = function()
       -- Optionally configure and load the colorscheme
       -- directly inside the plugin declaration.
-      vim.g.everforest_enable_italic = true
-      vim.g.everforest_background = 'hard'
-      vim.cmd.colorscheme 'everforest'
+      vim.g.gruvbox_material_enable_italic = true
+      vim.g.gruvbox_material_background = 'soft'
+      vim.g.gruvbox_material_foreground = 'original' -- default is 'material'
+      vim.cmd.colorscheme 'gruvbox-material'
     end,
   },
 }
